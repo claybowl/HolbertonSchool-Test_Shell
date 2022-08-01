22047aef11c38c1e8e0b7385ddff4862b8a8b418
VirtualBlakeWillis <bewillis101@gmail.com>
2022-07-31 17:55:23 -0700

    betty

--- shell.c
+++ shell.c
@@ -33,31 +33,31 @@ int shell(char **argv, int tty)
 	size_t buffsize = 0;
 	char *buff = NULL, *tmp = NULL; /**cmd = NULL*/
 	int valid_input = 1;
+
 	while (valid_input)
 	{
 		if (tty == 1)
 			printf("$ ");
-		
+
 		valid_input = getline(&buff, &buffsize, stdin);
 		if (_strcmp(buff, "exit\n") == 0)
 		{
 			break;
 		}
 		tmp = prep_string(buff);
-		
+
 		if (tmp == NULL)
 			perror(argv[0]);
 		/*
-		cmd = is_cmd_exist(tmp);
-		
-		if (cmd == NULL)
-			perror(argv[0]);
-		else
-		{
-			command(cmd);
-			if (_strcmp(tmp, cmd) != 0)
-				free(cmd);
-		}
+		 *cmd = is_cmd_exist(tmp);
+		 *if (cmd == NULL)
+		 *	perror(argv[0]);
+		 *else
+		 * {
+		 *	command(cmd);
+		 *	if (_strcmp(tmp, cmd) != 0)
+		 *		free(cmd);
+		 * }
 		*/
 		command(tmp);
 	}
